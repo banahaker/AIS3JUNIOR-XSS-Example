bd236cbcc1b1b5601a10f6c8a35568c65f0937c8
banahaker <pn0818x@outlook.com>
2022-08-25 11:18:44 +0800

    feat: login logic

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,62 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import axios from "axios";
+interface LoginForm {
+  account: string;
+  password: string;
+}
+
+interface loginResponse {
+  account: string;
+  id: number;
+  password: string;
+}
+
+const Logging = ref(false);
+
+const URL = "https://attackme.b0925138932.repl.co/login";
+const LoginInfo = ref<LoginForm>({
+  account: "",
+  password: "",
+});
+function login(data: LoginForm) {
+  if (data.account == "" || data.password == "") return;
+  Logging.value = true;
+  axios({
+    method: "POST",
+    url: URL,
+    data: data,
+  })
+    .then((res) => {
+      const data = res.data as loginResponse;
+      console.log(data);
+      localStorage.setItem("account", data.account);
+      localStorage.setItem("id", String(data.id));
+      localStorage.setItem("password", data.password);
+      Logging.value = false;
+      alert("Login Succeed");
+    })
+    .catch((err) => {
+      console.log(err.response.data);
+      const data = err.response.data;
+      alert("LoginFail\n" + data.message);
+      Logging.value = false;
+    });
+}
+</script>
+
 <template>
   <div class="login">
     <h1>login</h1>
-    <input type="text" placeholder="Username" />
-    <input type="password" name="psd" id="psd" placeholder="Password" />
+    <input v-model="LoginInfo.account" type="text" placeholder="Username" />
+    <input
+      v-model="LoginInfo.password"
+      type="password"
+      placeholder="Password"
+    />
     <p>Or <router-link to="/signup">SignUp</router-link></p>
-    <button>Login</button>
+    <button disabled v-if="Logging">Logging in</button>
+    <button v-else @click="login(LoginInfo)">Login</button>
   </div>
 </template>
 
